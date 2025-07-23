b34dcacc136d9394ceab9cbad13d9b6788607af9
Yusuf <teajhaney@gmail.com>
2025-07-23 17:07:19 +0100

    feat: enhance cookie handling by adding signed cookies and improving response messages

--- fullstack/cookies-demo/index.js
+++ fullstack/cookies-demo/index.js
@@ -2,16 +2,23 @@ const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
 
-app.use(cookieParser());
+app.use(cookieParser('password'));
 
 app.get('/', (req, res) => {
-	const {name}= req.cookies;
-  res.send(`${name} Welcome to the cookies DEMO`);
+  const { name, fruit } = req.cookies;
+  res.send(
+    `${name} Welcome to the cookies DEMO, I know your favorite fruit is ${fruit}`
+  );
 });
 
 app.get('/setname', (req, res) => {
-  res.cookie('name', 'teajhaney');
+  res.cookie('name', 'Yusuf');
   res.send('Name cookie set');
+});
+
+app.get('/getsignedcookie', (req, res) => {
+  res.cookie('fruit', 'apple', { signed: true });
+  res.send('Signed cookie for fruit set');
 });
 app.listen(3000, () => {
   console.log(`server started on port:  3000`);
