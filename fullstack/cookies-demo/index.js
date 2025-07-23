c46a95d8c76e7cb6357e9a3d56d4555302842c06
Yusuf <teajhaney@gmail.com>
2025-07-23 17:48:03 +0100

    fix: separate cookie retrieval for signed and regular cookies in the root route

--- fullstack/cookies-demo/index.js
+++ fullstack/cookies-demo/index.js
@@ -5,7 +5,8 @@ const cookieParser = require('cookie-parser');
 app.use(cookieParser('password'));
 
 app.get('/', (req, res) => {
-  const { name, fruit } = req.cookies;
+  const { name } = req.cookies;
+  const { fruit } = req.signedCookies;
   res.send(
     `${name} Welcome to the cookies DEMO, I know your favorite fruit is ${fruit}`
   );
@@ -15,7 +16,7 @@ app.get('/setname', (req, res) => {
   res.cookie('name', 'Yusuf');
   res.send('Name cookie set');
 });
-
+ 
 app.get('/getsignedcookie', (req, res) => {
   res.cookie('fruit', 'apple', { signed: true });
   res.send('Signed cookie for fruit set');
