809a07b74a7ed787a66b41603b117f3f2e3e9b87
Yusuf <teajhaney@gmail.com>
2025-07-25 23:37:42 +0100

    feat: implement user registration and login functionality with bcrypt hashing

--- fullstack/auth-demo/index.js
+++ fullstack/auth-demo/index.js
@@ -1,24 +1,67 @@
-const bcrypt = require('bcrypt');
+const express = require('express');
+const app = express();
+const path = require('path');
+const User = require('./model/user');
+const connectDB = require('./mongoose');
+const { hashPassword, comparePassword } = require('./utils');
+connectDB();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-const hashPassword = async password => {
-  const saltRounds = 12;
-  const salt = await bcrypt.genSalt(saltRounds);
-  const hashedPassword = await bcrypt.hash(password, salt);
-  console.log(salt);
-  console.log(hashedPassword);
-};
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-// hashPassword('yusuf1234');
-const login = async (password, hashedPassword) => {
-  const isMatch = await bcrypt.compare(password, hashedPassword);
-  if (isMatch) {
-    console.log('Password matches!');
-  } else {
-    console.log('Password does not match.');
+app.get('/', (req, res) => {
+  res.send('This is the home page');
+});
+
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const hashedPassword = await hashPassword(password);
+    const user = new User({
+      username: username,
+      password: hashedPassword,
+    });
+    await user.save();
+    res.redirect('/');
+  } catch (err) {
+    console.error('Registration failed:', err);
+    res.status(500).send('Something went wrong during registration.');
   }
-};
+});
 
-login(
-  'yusuf1234',
-  '$2b$12$GxQew45n7k0LJa7LyUPOCumn1To3V6CKUiBAFJNcDwbCZEu014u4i'
-); //
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ username });
+    const isPasswordValid = await comparePassword(password, user.password);
+    if (!user) {
+      return res.status(400).send('User not found.');
+    }
+    if (!isPasswordValid) {
+      return res.status(401).send('Invalid username or password.');
+    }
+
+    res.redirect('/secret');
+  } catch (err) {
+    console.error('Login failed:', err);
+    return res.status(500).send('Something went wrong during login.');
+  }
+});
+
+app.get('/secret', async (req, res) => {
+  res.send('This is a secret page, can be viewd by logged in users only!!');
+});
+
+app.listen(3000, () => {
+  console.log(`server started on port:  3000`);
+});
