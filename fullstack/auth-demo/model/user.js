34ecff113566bed7c80361add61f9a304752237e
Yusuf <teajhaney@gmail.com>
2025-07-26 01:42:30 +0100

    fix: ensure password hashing only occurs on password modification

--- fullstack/auth-demo/model/user.js
+++ fullstack/auth-demo/model/user.js
@@ -23,7 +23,9 @@ userSchema.statics.findByUsername = async function (username, password) {
 };
 
 userSchema.pre('save', async function (next) {
-  this.password = await hashPassword(this.password);
+  if (this.isModified('password')) {
+    this.password = await hashPassword(this.password);
+  }
   next();
 });
 
