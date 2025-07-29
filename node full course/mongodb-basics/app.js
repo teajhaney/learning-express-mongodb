f16b5ae996bca25f7cea0ac4bb9a7824300bf499
Yusuf <teajhaney@gmail.com>
2025-07-29 15:29:54 +0100

    feat: add basic book management API with CRUD operations

---
+++ node full course/mongodb-basics/app.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const { Schema, model } = require('mongoose');
+mongoose
+  .connect(
+    'mongodb+srv://teajhaney:futarian%4020@cluster0.csijp9n.mongodb.net/myDatabase?retryWrites=true&w=majority'
+  )
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch(err => {
+    console.error('Error connecting to MongoDB:', err);
+  });
+
+const userSchema = new Schema({
+  name: String,
+  email: String,
+  age: Number,
+  isActive: Boolean,
+  tags: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// create user model
+const User = model('User', userSchema);
+
+const runQueryExample = async () => {
+  try {
+    // const newUser = await User.create({
+    //   name: 'Tayo',
+    //   email: 'Tayo@gmail.com',
+    //   age: 20,
+    //   isActive: false,
+    //   tags: ['developer'],
+    // });
+    // console.log('created new user', newUser);
+    // const allUser = await User.find({});
+    // console.log(allUser);
+
+    // const falseUser = await User.find({ isActive: false });
+    // console.log(falseUser);
+    const selectedField = await User.find({}).select('name email -_id');
+    console.log(selectedField);
+  } catch (error) {
+    console.error('Error running query example:', error);
+  } finally {
+    mongoose.connection.close();
+  }
+};
+
+runQueryExample();
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
