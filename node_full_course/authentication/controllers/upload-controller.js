a40ab5b0d287277e7eb8c9446ca8e6d285dbf2a4
Yusuf <teajhaney@gmail.com>
2025-07-31 20:17:52 +0100

    refactor: streamline image upload and fetch functionality with asyncWrapper

--- node_full_course/authentication/controllers/upload-controller.js
+++ node_full_course/authentication/controllers/upload-controller.js
@@ -1,45 +1,45 @@
 const Image = require('../model/image');
 const { uplaodToCloudinary } = require('../helpers/cloudinary-helpers');
+const { asyncWrapper } = require('../utils');
 const fs = require('fs');
 
-const imageUpload = async (req, res) => {
-  try {
-    //check if file is available in req
-
-    if (!req.file) {
-      return res.status(400).json({
-        success: false,
-        message: 'No file uploaded. Please upload an image.',
-      });
-    }
-
-    //upload to cloudinary
-
-    const { url, publicID } = await uplaodToCloudinary(req.file.path);
-
-    //store image url and public ID to mongodb
-    const uplaodedImage = await Image.create({
-      url,
-      publicID,
-      uploadBy: req.userInfo.userID,
-    });
-
-    //delete from local Storage
-
-    fs.unlinkSync(req.file.path);
-
-    res.status(201).json({
-      success: true,
-      message: 'Image uploaded successfully',
-      data: uplaodedImage,
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
+const imageUpload = asyncWrapper(async (req, res) => {
+  //check if file is available in req
+  if (!req.file) {
+    return res.status(400).json({
       success: false,
-      message: 'Something went wrong! Please try again',
+      message: 'No file uploaded. Please upload an image.',
     });
   }
-};
 
-module.exports = { imageUpload };
+  //upload to cloudinary
+  const { url, publicID } = await uplaodToCloudinary(req.file.path);
+
+  //store image url and public ID to mongodb
+  const uplaodedImage = await Image.create({
+    url,
+    publicID,
+    uploadBy: req.userInfo.userID,
+  });
+
+  //delete from local Storage
+  fs.unlinkSync(req.file.path);
+
+  res.status(201).json({
+    success: true,
+    message: 'Image uploaded successfully',
+    data: uplaodedImage,
+  });
+});
+
+const fetchAllImage = asyncWrapper(async (req, res) => {
+  const allImages = await Image.find();
+  if (allImages) {
+    res.status(200).json({
+      succues: true,
+      data: allImages,
+    });
+  }
+});
+
+module.exports = { imageUpload, fetchAllImage };
--- node_full_course/authentication/routes/image-routes.js
+++ node_full_course/authentication/routes/image-routes.js
@@ -2,23 +2,16 @@ const express = require('express');
 const authMiddleware = require('../middleware/auth-middleware');
 const adminMiddleware = require('../middleware/admin-middleware');
 const uploadMiddleware = require('../middleware/uplaod-middleware');
-const { imageUpload } = require('../controllers/upload-controller');
-const { asyncWrapper } = require('../utils');
+const {
+  imageUpload,
+  fetchAllImage,
+} = require('../controllers/upload-controller');
+
 const Image = require('../model/image');
 const router = express.Router();
 
 //get images
-router.get(
-  '/',
-  authMiddleware,
-  asyncWrapper(async (req, res) => {
-    const AllImages = await Image.find();
-    res.status(200).json({
-      succues: true,
-      data: AllImages,
-    });
-  })
-);
+router.get('/', authMiddleware, fetchAllImage);
 
 //uplaod image
 router.post(
