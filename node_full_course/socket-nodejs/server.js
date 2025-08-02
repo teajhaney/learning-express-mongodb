d25076a82a86b738ee0e4b5774488970d5acca83
Yusuf <teajhaney@gmail.com>
2025-08-02 23:15:47 +0100

    feat: implement chat application with user management and message handling

---
+++ node_full_course/socket-nodejs/server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const socketIO = require('socket.io');
+
+//initiate socket.io and attcged this to our http server
+const io = socketIO(server);
+
+app.use(express.static('public'));
+
+const users = new Set();
+
+io.on('connection', socket => {
+  console.log('A user is now connected');
+
+  //handle users when they join the chat
+  socket.on('join', userName => {
+    users.add(userName);
+    socket.userName = userName;
+    io.emit('userJoined', userName);
+    io.emit('usersList', Array.from(users));
+  });
+
+  // handle incoming chat messages
+  socket.on('sendMessage', ({ message, userName }) => {
+    // broadcast message to all connected user
+    //    socket.broadcast.emit('messages', chatMessages);
+    io.emit('newMessage', { message, userName });
+  });
+
+  //handle disconnection
+  socket.on('disconnect', () => {
+    console.log('A user is now disconnected');
+    users.forEach(user => {
+      if ((user = socket.userName)) {
+        users.delete(socket.userName);
+        io.emit('userLeft', user);
+        io.emit('usersList', Array.from(users));
+      }
+    });
+  });
+});
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
